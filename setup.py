40131a81c4a686afbfae763673a78512ef73f9f0
Jeong Wook Oh <oh.jeongwook@gmail.com>
2019-12-28 14:51:38 -0800

    Updated setup

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = "elasticsearch_hunting-ohjeongwook",
+    name = "threat_hunting_tools-ohjeongwook",
     version = "0.0.1",
     author = "Matt Oh",
     author_email = "jeongoh@darungrim.com",
     description = "Elasticsearch Hunting Scripts for sysmon/winlogbeat events",
     long_description = long_description,
     long_description_content_type = "text/markdown",
-    url = "https://github.com/ohjeongwook/ElasticSearchHunting",
+    url = "https://github.com/ohjeongwook/ThreatHuntingTools",
     packages = setuptools.find_packages(),
     install_requires = [
         'Cython',
