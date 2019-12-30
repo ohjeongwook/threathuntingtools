7cb644ee4d7a69dedc6beba20125e6ee5f9a7ab8
Jeong Wook Oh <oh.jeongwook@gmail.com>
2019-12-29 21:22:14 -0800

    Changed variable name

--- scripts/set_environment_variables.cmd
+++ scripts/set_environment_variables.cmd
@@ -1,4 +1,4 @@
-setx SCML_TOOLS %CD%\..\elasticsearch_hunting
+setx THREAT_HUNTING_TOOLS %CD%\..
 setx PYTHONPATH %CD%\..\elasticsearch_hunting;%PYTHONPATH%
 setx JUPYTER_PATH %CD%\..\elasticsearch_hunting;%JUPYTER_PATH%
 pause
