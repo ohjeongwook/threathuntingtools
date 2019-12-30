a3fc121669600e623f2bbf2a2cc5b81604210225
Jeong Wook Oh <oh.jeongwook@gmail.com>
2019-12-30 10:11:01 -0800

    Fixed namespace

--- tests/dump_powershell_scripts.py
+++ tests/dump_powershell_scripts.py
@@ -9,6 +9,6 @@ import threathunting.powershell
 start_datetime = datetime.datetime.strptime('2019-05-01 19:40:00.0', '%Y-%m-%d %H:%M:%S.%f')
 end_datetime = datetime.datetime.strptime('2019-07-29 19:50:00.0', '%Y-%m-%d %H:%M:%S.%f')
 
-powershell_telemetry = powershell.Telemetry(telemetry_server = 'localhost', start_datetime = start_datetime, end_datetime = end_datetime, scan = True)
+powershell_telemetry = threathunting.powershell.Telemetry(telemetry_server = 'localhost', start_datetime = start_datetime, end_datetime = end_datetime, scan = True)
 powershell_telemetry.analyze()
 powershell_telemetry.dump_script_blocks()
