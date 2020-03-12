cddcd696d00b3e097880f871b4fd2a86c2e79c53
Jeong Wook Oh <oh.jeongwook@gmail.com>
2020-03-12 10:30:53 -0700

    Fixed import usage

--- tests/dump_process_tree.py
+++ tests/dump_process_tree.py
@@ -11,6 +11,6 @@ if __name__ == '__main__':
     start_datetime = datetime.strptime('2019-05-20 19:40:00.0', '%Y-%m-%d %H:%M:%S.%f')
     end_datetime = datetime.strptime('2019-05-20 19:50:00.0', '%Y-%m-%d %H:%M:%S.%f')
     
-    processes = process.Processes(hostname = "NEWSTARTBASE", start_datetime = start_datetime, end_datetime = end_datetime, scan = True)
+    processes = threathunting.process.Processes(hostname = "NEWSTARTBASE", start_datetime = start_datetime, end_datetime = end_datetime, scan = True)
     process_trees = processes.build_tree()
     process_trees.print()
